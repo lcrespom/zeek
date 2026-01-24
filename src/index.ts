a1e25339311b900c15b78101e00f5cb434d82f9f
Luis Crespo <lcrespom@gmail.com>
2026-01-24 18:50:54 +0100

    Removed breathing space in dir history to temporarily disable bug

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ function main() {
       addCwdToHistory()
       break
     case 'dir-history':
-      const dirPopup = new MenuPopup(getDirHistory().map(d => d + ' '))
+      const dirPopup = new MenuPopup(getDirHistory())
       dirPopup.openMenuPopup(lbuffer, rbuffer)
       break
     case 'file-search':
